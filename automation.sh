1553f4a8a8c154d352f5614d6b6ff02bbaaa5672
srikarimmidisetty <30678063+srikarimmidisetty@users.noreply.github.com>
2023-03-02 18:32:01 +0530

    Update automation.sh
    This is the final commit for task 2 and task 3

--- automation.sh
+++ automation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo apt update -y
-#sudo apt install apache2
-#sudo systemctl enable apache2
+sudo apt update -y
+sudo apt install apache2
+sudo systemctl enable apache2
 timestamp=$(date '+%d%m%Y-%H%M%S')
 name='Srikar'
 s3_bucket="upgrad-srikar"
